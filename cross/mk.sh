520691959991290897d3f3f1ea382b0792704613
Nick Pratt <nick.pratt772@outlook.com>
2025-04-03 11:44:53 -0500

    clean up mk

--- cross/mk.sh
+++ cross/mk.sh
@@ -5,10 +5,16 @@ if [[ "$(dirname $0)" != "." ]]; then
   exit 1
 fi
 
+# set the project directory
 rootdir=$(pwd)
 
-# unzip zlib. The video uses zlib v1.2.11
-cd zlib
+# setup the target root file system
+mkdir -p $rootdir/rootfs/usr/local
+
+# zLib
+# 
+# Download tar
+# wget --no-clobber https://github.com/madler/zlib/archive/refs/tags/v1.2.11.tar.gz -O zlib/zlib-1.2.11.tar.gz
 tar -xf zlib-1.2.11.tar.gz
 
 # Configure zLib
@@ -18,9 +24,18 @@ tar -xf zlib-1.2.11.tar.gz
 #   - Remember the "install", "include", and "library" paths are all different locations
 #   - By placing the shell parameter, in this case "CC", before command we modify the command's enviroment.
 #   - "These assignment statements affect only the environment seen by that command." (Bash Manual 3.7.4)
+#   - Use `cat configure.log` and make sure the prefix is correct to avoid install files on your own system.
 # Arguments:
 #   CC          specifies the cross-compiler
 #   --prefix    specifies the install directory
 cd zlib-1.2.11
 mkdir -p $rootdir/rootfs/usr/local
-CC=arm-linux-gnueabi-gcc ./configure --prefix=/repos/personal/learn/cross-compile/cross/rootfs/usr/local
+CC=arm-linux-gnueabi-gcc ./configure --prefix=$rootdir/cross/rootfs/usr/local
+
+# 
+
+# OpenSSL
+# 
+# Download tar
+# wget --no-clobber https://github.com/openssl/openssl/releases/download/OpenSSL_1_1_1/openssl-1.1.1.tar.gz -O openssl/openssl-1.1.1.tar.gz
+
