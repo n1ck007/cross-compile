c6bfeddd61defa0e853c90c503d5fc9b28b0911a
Nick Pratt <nick.pratt772@outlook.com>
2025-04-03 12:47:30 -0500

    fixed

--- cross/clean.sh
+++ cross/clean.sh
@@ -4,3 +4,5 @@ if [[ "$(basename $(pwd))" != "cross" ]]; then
   printf "$(basename $0): This script must called from the directory it resides in.\n"
   exit 1
 fi
+
+rm -rf zlib/zlib-1.2.11 openssl/openssl-1.1.1 rootfs
